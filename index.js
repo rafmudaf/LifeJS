6f52bb27123186ec77f4e1fef0f691bcbac41ce0
Rafael M Mudafort <raf@Rafaels-MacBook-Pro-2.local>
2017-08-20 14:28:46 -0600

    better code organization
    - ui handling moved to ints own section

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+// This is the custom javscript for index.html
+
+// init function
 (function() {
-    $("#infinity-button").click(function () {
-        updateStatus('infinitely playing');
-    });
-    
-    $("#one-button").click(function () {
-        updateStatus("one generation played");
-    });
-    
-    $("#pause-button").click(function () {
-        updateStatus("paused");
-    });
+    setCanvasSize(500, 500, 20, 20);
 })();
 
-function updateGenerationCount(gen) {
-    $("#genCount").text(gen);
-}
+// UI handling
+$("#x-resolution-slider, #y-resolution-slider").slider();
+$("#x-resolution-slider, #y-resolution-slider").on("slide", updateMesh);
+$("#x-resolution-slider, #y-resolution-slider").on("slideStop", reset);
 
-function updateStatus(msg) {
-    $("#status").text(msg);
-}
+$('#lifesketch').click(paintAPixel);
+
+$("#infinity-button").click(() => { updateStatus('infinitely playing'); });
+$('#infinity-button').click(startIteration);
+
+$("#one-button").click(() => { updateStatus("one generation played"); });
+$('#one-button').click(oneIteration);
+
+$("#pause-button").click(() => { updateStatus("paused"); });
+$('#pause-button').click(stopIteration);
+
+$('#reset-button').click(reset);
+
 
 // var xdim = 500;
 // var ydim = 500;
@@ -34,6 +38,14 @@ var xpixSize;
 var ypixSize;
 var paused;
 
+function updateGenerationCount(gen) {
+    $("#genCount").text(gen);
+}
+
+function updateStatus(msg) {
+    $("#status").text(msg);
+}
+
 function setCanvasSize(xdimin, ydimin, xresin, yresin) {
     xdim = xdimin;
     ydim = ydimin;
@@ -42,17 +54,8 @@ function setCanvasSize(xdimin, ydimin, xresin, yresin) {
     xpixSize = xdim/xres;
     ypixSize = ydim/yres;
 }
-(function () {setCanvasSize(500, 500, 20, 20)})();
 
-// With JQuery
-$("#x-resolution-slider, #y-resolution-slider").slider();
-$("#x-resolution-slider, #y-resolution-slider").on("slide", function(slideEvt) {
-    updateMe();
-});
-$("#x-resolution-slider, #y-resolution-slider").on("slideStop", function(slideEvt) {
-    reset();
-});
-function updateMe() {
+function updateMesh() {
     var pjs = Processing.getInstanceById('lifesketch');
     
     var xcount = $("#x-resolution-slider").data("slider").getValue();
@@ -86,13 +89,13 @@ function updateMe() {
     pjs.updateMesh(1, resolution);
 }
 
-$('#lifesketch').click(paintAPixel);
 function paintAPixel(e) {
     var pjs = Processing.getInstanceById('lifesketch');
     var mouseXY = getMousePos(document.getElementById('lifesketch'), e);
     var pixelIndex = Math.floor(mouseXY.x/xpixSize)+Math.floor(mouseXY.y/ypixSize)*xres;
     pjs.fillPixel(pixelIndex);
 }
+
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -101,25 +104,21 @@ function getMousePos(canvas, evt) {
     }
 }
 
-$('#infinity-button').click(startIteration);
 function startIteration() {
     var pjs = Processing.getInstanceById('lifesketch');
     pjs.start();
 }
 
-$('#one-button').click(oneIteration);
 function oneIteration() {
     var pjs = Processing.getInstanceById('lifesketch');
     pjs.runFor(1);
 }
 
-$('#pause-button').click(stopIteration);
 function stopIteration() {
     var pjs = Processing.getInstanceById('lifesketch');
     pjs.stop();
 }
 
-$('#reset-button').click(reset);
 function reset() {
     var pjs = Processing.getInstanceById('lifesketch');
     pjs.setup();
